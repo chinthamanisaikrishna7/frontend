a07a05fb83622ebfc43815bfb1150ca0f66d1645
chinthamanisaikrishna7 <saikrishnaias2004@gmail.com>
2024-07-03 23:20:15 +0530

    learning javascript

---
+++ scipt.js
@@ -0,0 +1,15 @@
+let a1=10;
+console.log(a1);
+
+a1+=10;
+console.log(a1);
+a1-=10;
+console.log(a1);
+a1*=100;
+console.log(a1);
+a1/=10;
+console.log(a1);
+a1**=10;
+console.log(a1);
+a1%=10;
+console.log(a1);
